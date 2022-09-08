dbdf72f446b7716f2e52eaab8a3536a4f14e0592
ZakCannon <Zak.Cannon@softwire.com>
2022-09-08 15:05:06 +0100

    done some of part 2

--- bus_board.rb
+++ bus_board.rb
@@ -2,34 +2,67 @@ require 'httparty'
 require_relative 'make_url'
 require_relative 'response_obj'
 
-puts "Enter stop code: "
-stop_code = gets.chomp
 
-targ_req = Request.new("api.tfl.gov.uk", "StopPoint", stop_code, "Arrivals")
-request_url = targ_req.make_url
+puts "Enter post code (no spaces): "
+postcode = gets.chomp
+puts "Enter radius: "
+radius = gets.chomp
 
-dir_response = HTTParty.get(request_url)
-response_hash = JSON.parse(dir_response.body)
+postcodes_req = Request.new("api.postcodes.io", "postcodes/", postcode)
+pc_request_url = postcodes_req.make_url
 
-response_obj = StopPointResponseObj.new(response_hash)
+pc_dir_response = HTTParty.get(pc_request_url)
+pc_response_hash = JSON.parse(pc_dir_response.body)
 
-output_hash = response_obj.make_out_hash(5)
+loc_coords = [pc_response_hash["result"]["latitude"], pc_response_hash["result"]["longitude"]]
 
-for i in 0..4 do
+tfl_loc_search_qlist = APIQueryList.new({"stopTypes" => "NaptanPublicBusCoachTram", "modes" => "bus", "radius" => radius, "lon" => loc_coords[1], "lat" => loc_coords[0]})
+tfl_loc_search_outlist = tfl_loc_search_qlist.make_list
+tfl_loc_search_req = Request.new("api.tfl.gov.uk", "StopPoint", tfl_loc_search_outlist)
+tfl_loc_search_url = tfl_loc_search_req.make_url
 
-  if i == 0
-    puts "Next bus details:"
+
+tfl_loc_search_response = HTTParty.get(tfl_loc_search_url)
+tfl_loc_search_hash = JSON.parse(tfl_loc_search_response.body)
+stop_point_hash = tfl_loc_search_hash["stopPoints"]
+
+for i in 0..100 do
+
+  tfl_req = Request.new("api.tfl.gov.uk", "StopPoint/", stop_point_hash[i]["naptanId"], "Arrivals")
+  tfl_request_url = tfl_req.make_url
+  puts "Request url: #{tfl_request_url}"
+
+  tfl_dir_response = HTTParty.get(tfl_request_url)
+  tfl_response_hash = JSON.parse(tfl_dir_response.body)
+
+  if tfl_response_hash == []
+    puts "No departures found for stop #{stop_point_hash[i]["naptanId"]}"
+  else
+
+    tfl_response_obj = StopPointResponseObj.new(tfl_response_hash)
+
+    output_hash = tfl_response_obj.make_out_hash(5)
+    puts "For stop: #{stop_point_hash[i]["commonName"]} Which is #{stop_point_hash[i]["distance"]}m away:"
+
+    for j in 0..4 do
+
+      if j == 0
+        puts "Next bus details:"
+      end
+
+      if j == 1
+             puts "Later bus details..."
+      end
+
+      arrives_in = output_hash.keys[j]
+
+
+      puts "Arrives in: #{arrives_in}min"
+      puts "LineID: #{output_hash[arrives_in][0]}"
+      puts "Destination: #{output_hash[arrives_in][1]}"
+      puts "\n"
+    end
   end
-
-  if i == 1
-         puts "Later bus details"
-  end
-
-  arrives_in = output_hash.keys[i]
-
-  puts "Arrives in: #{arrives_in}min"
-  puts "LineID: #{output_hash[arrives_in][0]}"
-  puts "Destination: #{output_hash[arrives_in][1]}"
-  puts "\n"
 end
+
 
--- make_url.rb
+++ make_url.rb
@@ -9,7 +9,22 @@ class Request
 
   def make_url
     args_string = @args.join("/")
-    fin_url = "https://#{@host}/#{@request_code}/#{args_string}"
+    fin_url = "https://#{@host}/#{@request_code}#{args_string}"
     return fin_url
+  end
+end
+
+class APIQueryList
+  attr_reader :args
+  def initialize(args)
+    @args = args
+  end
+
+  def make_list
+    out_list = "?"
+    args.each do |key, value|
+      out_list += "#{key}=#{value}&"
+    end
+    return out_list.delete_suffix!("&")
   end
 end
