1a3c0a69f8dc9956625da023c6f4265b2194c735
ZakCannon <Zak.Cannon@softwire.com>
2022-09-08 12:15:04 +0100

    part 1 done and working

--- bus_board.rb
+++ bus_board.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require_relative 'make_url'
+require_relative 'response_obj'
 
 puts "Enter stop code: "
 stop_code = gets.chomp
@@ -7,8 +8,28 @@ stop_code = gets.chomp
 targ_req = Request.new("api.tfl.gov.uk", "StopPoint", stop_code, "Arrivals")
 request_url = targ_req.make_url
 
-response = HTTParty.get(request_url)
-response_hash = JSON.parse(response.body)
+dir_response = HTTParty.get(request_url)
+response_hash = JSON.parse(dir_response.body)
 
-puts response_hash
+response_obj = StopPointResponseObj.new(response_hash)
+
+output_hash = response_obj.make_out_hash(5)
+
+for i in 0..4 do
+
+  if i == 0
+    puts "Next bus details:"
+  end
+
+  if i == 1
+         puts "Later bus details"
+  end
+
+  arrives_in = output_hash.keys[i]
+
+  puts "Arrives in: #{arrives_in}min"
+  puts "LineID: #{output_hash[arrives_in][0]}"
+  puts "Destination: #{output_hash[arrives_in][1]}"
+  puts "\n"
+end
 
---
+++ response_obj.rb
@@ -0,0 +1,37 @@
+class StopPointResponseObj
+  attr_reader :response_hash
+
+  def initialize(response_hash)
+    @response_hash = response_hash
+  end
+
+  def get_bus_dest(num)
+    return @response_hash[num]["destinationName"]
+  end
+
+  def get_bus_lineid(num)
+    return @response_hash[num]["lineId"]
+  end
+
+  def get_bus_arr_time_UTC(num)
+    arr_time =  @response_hash[num]["expectedArrival"]
+    return arr_time[11, 8]
+  end
+
+  def get_time_to_arr(num)
+    return @response_hash[num]["timeToStation"]/60
+  end
+
+  def make_out_hash(num)
+    output_hash = {}
+
+    for i in 0..(num-1) do
+      output_hash[self.get_time_to_arr(i)]= [self.get_bus_lineid(i), self.get_bus_dest(i)]
+    end
+
+    output_sorted = output_hash.sort
+    output_hash_sorted = output_sorted.to_h
+
+    return output_hash_sorted
+  end
+end
